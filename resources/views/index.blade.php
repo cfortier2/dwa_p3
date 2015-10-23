0f72a8093d28dccea0b515eb83ae61d1e489f5cb
Chris Fortier <cfortier2@gmail.com>
2015-10-22 22:38:07 -0400

    index.blade.php: add some text

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts.master')
 
 @section('title')
-  My Title
+  Lorem Ipsum Generator
 @stop
 
 @section('content')
-<h1>Yo</h1>
+  <h1>Developer's Best Friend</h1>
+
+  <h1>Lorem Ipsum Generator</h1>
+  <h2>According to <a href=https://en.wikipedia.org/wiki/Lorem_ipsum>Wikipedia</a> lorem ipsum is defined as:</h2>
+  <p>In publishing and graphic design, lorem ipsum (derived from Latin dolorem ipsum, translated as "pain itself") is a filler text commonly used to demonstrate the graphic elements of a document or visual presentation. Replacing meaningful content with placeholder text allows viewers to focus on graphic aspects such as font, typography, and page layout without being distracted by the content. It also reduces the need for the designer to come up with meaningful text, as they can instead use quickly-generated lorem ipsum.</p>
+
+  <h1>Random User Generator</h1>
 @stop
